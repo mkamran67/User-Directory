c255f2adcfb5103bf258352c15c46779197544ce
Muhammad Kamran <mkamran1967@gmail.com>
2020-12-09 01:24:35 -0800

    Updates

---
+++ precache-manifest.80dd912259c716d30cbfb6c6530a67c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5ab8ba949ae557e1cea1b58b387948",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "d3b81c1b7b9e3e78a772",
+    "url": "/User-Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cfec7be00cc5b0492ec8",
+    "url": "/User-Directory/static/css/main.d2dea277.chunk.css"
+  },
+  {
+    "revision": "d3b81c1b7b9e3e78a772",
+    "url": "/User-Directory/static/js/2.e56c4717.chunk.js"
+  },
+  {
+    "revision": "cfec7be00cc5b0492ec8",
+    "url": "/User-Directory/static/js/main.771e0b7e.chunk.js"
+  },
+  {
+    "revision": "1a48e735230d9091ff90",
+    "url": "/User-Directory/static/js/runtime~main.2facd29f.js"
+  }
+]);
